aa9d1d806592ca2ac60171c52de96d95db8df8e7
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2021-10-29 08:04:28 +0200

    Add docs to ComponentVariableType

--- src/v2_0_1/datatypes/component_variable_type.rs
+++ src/v2_0_1/datatypes/component_variable_type.rs
@@ -1,12 +1,14 @@
+//! Struct to report components, variables and variable attributes and characteristics.
 use super::component_type::ComponentType;
 use super::variable_type::VariableType;
 
-/// Class to report components, variables and variable attributes and characteristics.
-/// ComponentVariableType is used by: GetMonitoringReportRequest , GetReportRequest
+///    ComponentVariableType is used by: GetMonitoringReportRequest , GetReportRequest
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct ComponentVariableType {
+    /// Required. Component for which a report of Variable is requested.
     pub component: ComponentType,
+    /// Optional. Variable(s) for which the report is requested.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub variable: Option<VariableType>,
 }
