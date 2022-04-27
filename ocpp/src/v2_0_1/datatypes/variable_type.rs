bcb96630aca36006ac16a0a99943f42b5369c6e6
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2022-04-27 19:45:20 +0200

    Add validation on fields

--- ocpp/src/v2_0_1/datatypes/variable_type.rs
+++ ocpp/src/v2_0_1/datatypes/variable_type.rs
@@ -1,9 +1,12 @@
+use validator::Validate;
 /// Reference key to a component-variable.
 /// VariableType is used by: Common:ComponentVariableType , GetVariablesRequest.GetVariableDataType , GetVariablesResponse.GetVariableResultType , NotifyMonitoringReportRequest.MonitoringDataType , NotifyReportRequest.ReportDataType , SetVariableMonitoringRequest.SetMonitoringDataType , SetVariableMonitoringResponse.SetMonitoringResultType , SetVariablesRequest.SetVariableDataType , SetVariablesResponse.SetVariableResultType , NotifyEventRequest.EventDataType
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct VariableType {
+    #[validate(length(min = 0, max = 50))]
     pub name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(length(min = 0, max = 50))]
     pub instance: Option<String>,
 }
