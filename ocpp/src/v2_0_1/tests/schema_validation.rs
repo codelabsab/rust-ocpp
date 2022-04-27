4e4d65cddbc63178bec97a1a3d536e383bfbbc88
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2022-04-27 12:57:36 +0200

    Add schema validation test
    - BootNotificationResponse

--- ocpp/src/v2_0_1/tests/schema_validation.rs
+++ ocpp/src/v2_0_1/tests/schema_validation.rs
@@ -2,12 +2,16 @@
 mod tests {
     use crate::v2_0_1::datatypes::charging_station_type::ChargingStationType;
     use crate::v2_0_1::enumerations::boot_reason_enum_type::BootReasonEnumType;
-    use crate::v2_0_1::messages::boot_notification::BootNotificationRequest;
+    use crate::v2_0_1::enumerations::registration_status_enum_type::RegistrationStatusEnumType;
+    use crate::v2_0_1::messages::boot_notification::{
+        BootNotificationRequest, BootNotificationResponse,
+    };
+    use chrono::Utc;
     use jsonschema::is_valid;
 
     #[test]
     fn schema_validation_test() {
-        let bnr = BootNotificationRequest {
+        let boot_notification_request = BootNotificationRequest {
             reason: BootReasonEnumType::PowerUp,
             charging_station: ChargingStationType {
                 model: "SingleSocketCharger".to_string(),
@@ -20,7 +24,19 @@ mod tests {
 
         let schema = include_str!("../../../../schemas/ocpp/v2.0.1/BootNotificationRequest.json");
         let schema = serde_json::from_str(&schema).unwrap();
-        let instance = serde_json::to_value(&bnr).unwrap();
+        let instance = serde_json::to_value(&boot_notification_request).unwrap();
+        assert!(is_valid(&schema, &instance));
+
+        let boot_notification_response = BootNotificationResponse {
+            current_time: Utc::now(),
+            interval: 10,
+            status: RegistrationStatusEnumType::Accepted,
+            status_info: None,
+        };
+
+        let schema = include_str!("../../../../schemas/ocpp/v2.0.1/BootNotificationResponse.json");
+        let schema = serde_json::from_str(&schema).unwrap();
+        let instance = serde_json::to_value(&boot_notification_response).unwrap();
         assert!(is_valid(&schema, &instance));
     }
 }
