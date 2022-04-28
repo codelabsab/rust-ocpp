8d6a4846176384d61002b5ff2c25830505570418
Taras Baziuk <baziuk.tar@gmail.com>
2022-04-28 17:52:24 +0300

    Make fields public

--- ocpp/src/v1_6/types/authorization_data.rs
+++ ocpp/src/v1_6/types/authorization_data.rs
@@ -6,8 +6,8 @@ use validator::Validate;
 pub struct AuthorizationData {
     /// Required. The identifier to which this authorization applies.
     #[validate(length(min = 1, max = 20))]
-    id_tag: String,
+    pub id_tag: String,
     /// Optional. (Required when UpdateType is Full) This contains information about authorization status, expiry and parent id. For a Differential update the following applies: If this element is present, then this entry SHALL be added or updated in the Local Authorization List. If this element is absent, than the entry for this idtag in the Local Authorization List SHALL be deleted.
     #[serde(skip_serializing_if = "Option::is_none")]
-    id_tag_info: Option<IdTagInfo>,
+    pub id_tag_info: Option<IdTagInfo>,
 }
--- ocpp/src/v1_6/types/key_value.rs
+++ ocpp/src/v1_6/types/key_value.rs
@@ -5,11 +5,11 @@ use validator::Validate;
 pub struct KeyValue {
     /// Required.
     #[validate(length(min = 1, max = 50))]
-    key: String,
+    pub key: String,
     /// Required. False if the value can be set with the ChangeConfiguration message.
-    readonly: bool,
+    pub readonly: bool,
     /// Optional. If key is known but not set, this field may be absent.
     #[validate(length(min = 1, max = 500))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    value: Option<String>,
+    pub value: Option<String>,
 }
