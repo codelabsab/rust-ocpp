27512984f0707dfd106f99a375d315ba1ff86e5d
Valdemar Erk <VER@nervesmartsystems.com>
2023-08-16 08:30:40 +0200

    fix: validate_string_identifier would allow some disallowed strings
    Any string containing at least one valid character would be allowed by
    the regex, this fix removes regex and does a normal scan of the string
    instead.
    
    This patch also contains some cleaning in the Cargo.toml file where it
    removes dependencies that was not in use.

--- src/v2_0_1/helpers/validator.rs
+++ src/v2_0_1/helpers/validator.rs
@@ -1,5 +1,12 @@
-use regex::Regex;
 use validator::ValidationError;
+
+/// Sorted list of characters allowed in `identifierString`.
+pub const ALLOWED_CHARACTERS: [char; 71] = [
+    '*', '+', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '=', '@', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|',
+];
 
 /// Helper function to validate identifierString
 ///
@@ -7,13 +14,44 @@ use validator::ValidationError;
 /// This is a case-insensitive dataType and can only contain characters from the following
 /// character set: `a-z`, `A-Z`, `0-9`, `'*'`, `'-'`, `'_'`, `'='`, `':'`, `'+'`, `'|'`, `'@'`, `'.'`
 pub fn validate_identifier_string(s: &str) -> Result<(), ValidationError> {
-    // regex for identifierString as defined by the specification
-    let re = Regex::new(r"[a-z]|[A-Z]|[0-9]|[+]|[*]|[-]|[=]|[:]|[0]|[|]|[@]|[.]").unwrap();
+    for c in s.chars() {
+        if !ALLOWED_CHARACTERS.contains(&c) {
+            return Err(ValidationError::new("Not a valid identifierString"));
+        }
+    }
 
-    let res = re.is_match(s);
+    Ok(())
+}
 
-    match res {
-        true => Ok(()),
-        false => Err(ValidationError::new("Not a valid identifierString")),
+#[cfg(test)]
+mod test {
+    use super::validate_identifier_string;
+
+    #[test]
+    fn good_case() {
+        let good_cases = ["abc123", "A*C_|..", "||||", "ABCabc123:==@"];
+
+        for case in good_cases.iter() {
+            dbg!(case);
+            validate_identifier_string(case).unwrap();
+        }
+    }
+
+    #[test]
+    fn bad_case() {
+        let bad_cases = [
+            "abc123/",
+            "https://",
+            "ABC#123",
+            ",,,,",
+            "Test test",
+            "123 Prøve",
+            "123 Test?",
+        ];
+
+        for case in bad_cases.iter() {
+            dbg!(case);
+            validate_identifier_string(case).unwrap_err();
+        }
     }
 }
