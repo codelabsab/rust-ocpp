ce6036f12f379d66231ffd8d5b81bcbecdbd39cc
Erk <Erk-@users.noreply.github.com>
2023-08-29 12:52:18 +0200

    fix: validate_string_identifier would allow some disallowed strings (#24)
    * fix: validate_string_identifier would allow some disallowed strings
    
    Any string containing at least one valid character would be allowed by
    the regex, this fix removes regex and does a normal scan of the string
    instead.
    
    This patch also contains some cleaning in the Cargo.toml file where it
    removes dependencies that was not in use.
    
    * use regex for validation
    
    * allow empty identifierString.

--- src/v2_0_1/helpers/validator.rs
+++ src/v2_0_1/helpers/validator.rs
@@ -1,5 +1,9 @@
+use std::sync::OnceLock;
+
 use regex::Regex;
 use validator::ValidationError;
+
+static REGEX: OnceLock<Regex> = OnceLock::new();
 
 /// Helper function to validate identifierString
 ///
@@ -8,12 +12,45 @@ use validator::ValidationError;
 /// character set: `a-z`, `A-Z`, `0-9`, `'*'`, `'-'`, `'_'`, `'='`, `':'`, `'+'`, `'|'`, `'@'`, `'.'`
 pub fn validate_identifier_string(s: &str) -> Result<(), ValidationError> {
     // regex for identifierString as defined by the specification
-    let re = Regex::new(r"[a-z]|[A-Z]|[0-9]|[+]|[*]|[-]|[=]|[:]|[0]|[|]|[@]|[.]").unwrap();
-
-    let res = re.is_match(s);
+    let res = REGEX
+        .get_or_init(|| Regex::new(r"^[a-zA-Z0-9*+=:|@._-]*$").unwrap())
+        .is_match(s);
 
     match res {
         true => Ok(()),
         false => Err(ValidationError::new("Not a valid identifierString")),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::validate_identifier_string;
+
+    #[test]
+    fn good_case() {
+        let good_cases = ["abc123", "A*C_|..", "||||", "ABCabc123:==@"];
+
+        for case in good_cases.iter() {
+            dbg!(case);
+            validate_identifier_string(case).unwrap();
+        }
+    }
+
+    #[test]
+    fn bad_case() {
+        let bad_cases = [
+            "abc123/",
+            "https://",
+            "ABC#123",
+            ",,,,",
+            "Test test",
+            "123 Prøve",
+            "123 Test?",
+        ];
+
+        for case in bad_cases.iter() {
+            dbg!(case);
+            validate_identifier_string(case).unwrap_err();
+        }
     }
 }
