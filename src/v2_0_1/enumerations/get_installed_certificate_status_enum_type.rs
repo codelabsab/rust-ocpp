64b1927e13088e6d6cb93c2fcc9bf085f06621e9
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2024-08-15 22:50:58 +0200

    Fix for type error
    Fixes #90

--- src/v2_0_1/enumerations/get_installed_certificate_status_enum_type.rs
+++ src/v2_0_1/enumerations/get_installed_certificate_status_enum_type.rs
@@ -1,4 +1,4 @@
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Default)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Default, PartialEq)]
 pub enum GetInstalledCertificateStatusEnumType {
     #[default]
     Accepted,
--- src/v2_0_1/messages/get_installed_certificate_ids.rs
+++ src/v2_0_1/messages/get_installed_certificate_ids.rs
@@ -2,7 +2,7 @@
 use crate::v2_0_1::datatypes::certificate_hash_data_chain_type::CertificateHashDataChainType;
 use crate::v2_0_1::datatypes::status_info_type::StatusInfoType;
 use crate::v2_0_1::enumerations::get_certificate_id_use_enum_type::GetCertificateIdUseEnumType;
-use crate::v2_0_1::enumerations::get_display_messages_status_enum_type::GetDisplayMessagesStatusEnumType;
+use crate::v2_0_1::enumerations::get_installed_certificate_status_enum_type::GetInstalledCertificateStatusEnumType;
 
 /// Used by the CSMS to request an overview of the installed certificates on a Charging Station.
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Default)]
@@ -18,7 +18,7 @@ pub struct GetInstalledCertificateIdsRequest {
 #[serde(rename_all = "camelCase")]
 pub struct GetInstalledCertificateIdsResponse {
     /// Charging Station indicates if it can process therequest
-    pub status: GetDisplayMessagesStatusEnumType,
+    pub status: GetInstalledCertificateStatusEnumType,
     /// The Charging Station includes the Certificateinformation for each available certificate.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub certificate_hash_data_chain: Option<Vec<CertificateHashDataChainType>>,
