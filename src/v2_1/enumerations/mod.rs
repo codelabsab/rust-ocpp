f8ab3721e2908e771e196c20b663c468b771a203
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-12 22:40:39 +0100

    feat: Add HashAlgorithmEnumType for OCPP v2.1
    - Introduce new HashAlgorithmEnumType enumeration
    - Support serialization and deserialization of hash algorithms
    - Include SHA256, SHA384, and SHA512 variants
    - Update enumerations module to export the new type

--- src/v2_1/enumerations/mod.rs
+++ src/v2_1/enumerations/mod.rs
@@ -1,7 +1,9 @@
 mod boot_reason;
 mod generic_status;
+mod hash_algorithm;
 mod registration_status;
 
 pub use boot_reason::BootReasonEnumType;
 pub use generic_status::GenericStatusEnumType;
+pub use hash_algorithm::HashAlgorithmEnumType;
 pub use registration_status::RegistrationStatusEnumType;
