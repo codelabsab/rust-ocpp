7c1256f4881e1fc581011de4e524ff3c59f403e7
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-12 22:42:06 +0100

    feat: Add AuthorizationStatusEnumType for OCPP v2.1
    - Introduce new AuthorizationStatusEnumType enumeration
    - Support serialization and deserialization of authorization statuses
    - Include multiple status variants like Accepted, Blocked, Expired, etc.
    - Update enumerations module to export the new type

--- src/v2_1/enumerations/mod.rs
+++ src/v2_1/enumerations/mod.rs
@@ -1,8 +1,10 @@
+mod authorization_status;
 mod boot_reason;
 mod generic_status;
 mod hash_algorithm;
 mod registration_status;
 
+pub use authorization_status::AuthorizationStatusEnumType;
 pub use boot_reason::BootReasonEnumType;
 pub use generic_status::GenericStatusEnumType;
 pub use hash_algorithm::HashAlgorithmEnumType;
