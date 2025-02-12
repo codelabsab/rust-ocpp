08bd39126e5fa9ebbaf9a485f9f3e0a08b21be85
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-12 22:39:20 +0100

    feat: Add GenericStatusEnumType for OCPP v2.1
    - Introduce new GenericStatusEnumType enumeration
    - Support serialization and deserialization of status
    - Add Accepted and Rejected status variants
    - Update enumerations module to export the new type

--- src/v2_1/enumerations/mod.rs
+++ src/v2_1/enumerations/mod.rs
@@ -1,5 +1,7 @@
 mod boot_reason;
+mod generic_status;
 mod registration_status;
 
 pub use boot_reason::BootReasonEnumType;
+pub use generic_status::GenericStatusEnumType;
 pub use registration_status::RegistrationStatusEnumType;
