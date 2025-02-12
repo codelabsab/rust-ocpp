c16c8744b47bbee2a65e238470918d2acfddafee
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-12 22:43:23 +0100

    feat: Add CertificateSigningUseEnumType for OCPP v2.1
    - Introduce new CertificateSigningUseEnumType enumeration
    - Support serialization and deserialization of certificate signing use types
    - Include variants for Charging Station, V2G, and V2G 2.0 certificates
    - Update enumerations module to export the new type

--- src/v2_1/enumerations/mod.rs
+++ src/v2_1/enumerations/mod.rs
@@ -1,11 +1,13 @@
 mod authorization_status;
 mod boot_reason;
+mod certificate_signing_use;
 mod generic_status;
 mod hash_algorithm;
 mod registration_status;
 
 pub use authorization_status::AuthorizationStatusEnumType;
 pub use boot_reason::BootReasonEnumType;
+pub use certificate_signing_use::CertificateSigningUseEnumType;
 pub use generic_status::GenericStatusEnumType;
 pub use hash_algorithm::HashAlgorithmEnumType;
 pub use registration_status::RegistrationStatusEnumType;
