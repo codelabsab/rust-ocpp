6d6e993d5aa92027082843e8e10b26cb9ebba89f
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-12 22:44:14 +0100

    feat: Add OperationalStatusEnumType for OCPP v2.1

--- src/v2_1/enumerations/mod.rs
+++ src/v2_1/enumerations/mod.rs
@@ -3,6 +3,7 @@ mod boot_reason;
 mod certificate_signing_use;
 mod generic_status;
 mod hash_algorithm;
+mod operational_status;
 mod registration_status;
 
 pub use authorization_status::AuthorizationStatusEnumType;
@@ -10,4 +11,5 @@ pub use boot_reason::BootReasonEnumType;
 pub use certificate_signing_use::CertificateSigningUseEnumType;
 pub use generic_status::GenericStatusEnumType;
 pub use hash_algorithm::HashAlgorithmEnumType;
+pub use operational_status::OperationalStatusEnumType;
 pub use registration_status::RegistrationStatusEnumType;
