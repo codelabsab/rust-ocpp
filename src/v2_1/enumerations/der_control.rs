c9911ddb55809c4d6573e9180fffad929db2491a
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-13 08:02:47 +0100

    feat: Add DERControlEnumType for OCPP v2.1
    - Introduce new DERControlEnumType enumeration
    - Support serialization and deserialization of Distributed Energy Resource (DER) control types
    - Include variants for various control modes such as EnterService, FreqDroop, VoltVar, and more
    - Update enumerations module to export the new type

---
+++ src/v2_1/enumerations/der_control.rs
@@ -0,0 +1,50 @@
+use serde::{Deserialize, Serialize};
+
+/// Type of DER control.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum DERControlEnumType {
+    #[serde(rename = "EnterService")]
+    EnterService,
+    #[serde(rename = "FreqDroop")]
+    FreqDroop,
+    #[serde(rename = "FreqWatt")]
+    FreqWatt,
+    #[serde(rename = "FixedPFAbsorb")]
+    FixedPFAbsorb,
+    #[serde(rename = "FixedPFInject")]
+    FixedPFInject,
+    #[serde(rename = "FixedVar")]
+    FixedVar,
+    #[serde(rename = "Gradients")]
+    Gradients,
+    #[serde(rename = "HFMustTrip")]
+    HFMustTrip,
+    #[serde(rename = "HFMayTrip")]
+    HFMayTrip,
+    #[serde(rename = "HVMustTrip")]
+    HVMustTrip,
+    #[serde(rename = "HVMomCess")]
+    HVMomCess,
+    #[serde(rename = "HVMayTrip")]
+    HVMayTrip,
+    #[serde(rename = "LimitMaxDischarge")]
+    LimitMaxDischarge,
+    #[serde(rename = "LFMustTrip")]
+    LFMustTrip,
+    #[serde(rename = "LVMustTrip")]
+    LVMustTrip,
+    #[serde(rename = "LVMomCess")]
+    LVMomCess,
+    #[serde(rename = "LVMayTrip")]
+    LVMayTrip,
+    #[serde(rename = "PowerMonitoringMustTrip")]
+    PowerMonitoringMustTrip,
+    #[serde(rename = "VoltVar")]
+    VoltVar,
+    #[serde(rename = "VoltWatt")]
+    VoltWatt,
+    #[serde(rename = "WattPF")]
+    WattPF,
+    #[serde(rename = "WattVar")]
+    WattVar,
+}
