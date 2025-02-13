0cf02f4a68ba8bc88638253005c41b19ef4ed898
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-13 10:02:01 +0100

    refactor: Consolidate Authorize and CertificateSigned message types
    - Merge separate request and response files for Authorize and CertificateSigned messages
    - Update module to import and export types from consolidated files
    - Maintain existing functionality and structure of message types
    - Improve code organization in the messages module

--- src/v2_1/messages/authorize.rs
+++ src/v2_1/messages/authorize.rs
@@ -1,4 +1,5 @@
-use super::super::datatypes::{CustomDataType, IdTokenType};
+use super::super::datatypes::{CustomDataType, IdTokenInfoType, IdTokenType, TariffType};
+use super::super::enumerations::{AuthorizeCertificateStatusEnumType, EnergyTransferModeEnumType};
 use serde::{Deserialize, Serialize};
 
 /// Request to start a transaction with the given idToken.
@@ -57,4 +58,29 @@ pub enum HashAlgorithmEnumType {
     SHA384,
     #[serde(rename = "SHA512")]
     SHA512,
+}
+
+/// Response to an AuthorizeRequest.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct AuthorizeResponse {
+    /// Contains information about authorization status, expiry and group id.
+    pub id_token_info: IdTokenInfoType,
+
+    /// Certificate status information.
+    /// - if all certificates are valid: return 'Accepted'.
+    /// - if one of the certificates was revoked, return 'CertificateRevoked'.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub certificate_status: Option<AuthorizeCertificateStatusEnumType>,
+
+    /// List of allowed energy transfer modes the EV can choose from. If omitted this defaults to charging only.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub allowed_energy_transfer: Option<Vec<EnergyTransferModeEnumType>>,
+
+    /// The tariff that is applied to this session.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tariff: Option<TariffType>,
+
+    /// Optional custom data
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub custom_data: Option<CustomDataType>,
 }
--- src/v2_1/messages/mod.rs
+++ src/v2_1/messages/mod.rs
@@ -1,14 +1,14 @@
-mod authorize_request;
-mod authorize_response;
+mod authorize;
 mod boot_notification;
 mod cancel_reservation_request;
 mod cancel_reservation_response;
-mod certificate_signed_request;
-mod certificate_signed_response;
+mod certificate_signed;
 
-pub use authorize_request::{AuthorizeRequest, HashAlgorithmEnumType, OCSPRequestDataType};
-pub use authorize_response::AuthorizeResponse;
+pub use authorize::{
+    AuthorizeRequest, AuthorizeResponse, HashAlgorithmEnumType, OCSPRequestDataType,
+};
 pub use cancel_reservation_request::CancelReservationRequest;
 pub use cancel_reservation_response::CancelReservationResponse;
-pub use certificate_signed_request::CertificateSignedRequest;
-pub use certificate_signed_response::{CertificateSignedResponse, CertificateSignedStatusEnumType};
+pub use certificate_signed::{
+    CertificateSignedRequest, CertificateSignedResponse, CertificateSignedStatusEnumType,
+};
