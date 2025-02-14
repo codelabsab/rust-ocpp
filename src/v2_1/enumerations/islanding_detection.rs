5881b95f710cb84c4037f77093ca1220ecc8bfa4
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-14 12:35:30 +0100

    refactor: Improve IslandingDetectionEnumType serialization and naming
    - Update import style for serde traits
    - Add serde rename attributes for UVP_OVP and UFP_OFP enum variants
    - Adjust enum variant names to follow Rust naming conventions

--- src/v2_1/enumerations/islanding_detection.rs
+++ src/v2_1/enumerations/islanding_detection.rs
@@ -1,10 +1,14 @@
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Default)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Default)]
 pub enum IslandingDetectionEnumType {
     #[default]
     NoAntiIslandingSupport,
     RoCoF,
-    UVP_OVP,
-    UFP_OFP,
+    #[serde(rename = "UVP_OVP")]
+    UvpOvp,
+    #[serde(rename = "UFP_OFP")]
+    UfpOfp,
     VoltageVectorShift,
     ZeroCrossingDetection,
     OtherPassive,
