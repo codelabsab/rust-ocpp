9a75bfc1e9548b87270ae08028a00da181885949
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2025-02-16 16:52:47 +0100

    feat: Enhance CustomDataType with additional properties support
    - Add support for arbitrary vendor-specific properties in CustomDataType
    - Use HashMap<String, Value> to allow flexible custom data storage
    - Remove Hash trait to improve type flexibility
    - Update serialization to skip empty additional properties

--- src/v2_1/datatypes/custom_data.rs
+++ src/v2_1/datatypes/custom_data.rs
@@ -1,12 +1,19 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
 use validator::Validate;
 
 /// This class does not get 'AdditionalProperties = false' in the schema generation,
 /// so it can be extended with arbitrary JSON properties to allow adding custom data.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Validate)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct CustomDataType {
     /// Vendor-specific identifier
     #[validate(length(max = 255))]
     pub vendor_id: String,
+
+    /// Additional vendor-specific properties
+    #[serde(flatten)]
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    pub additional_properties: HashMap<String, Value>,
 }
