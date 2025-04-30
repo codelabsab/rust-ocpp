5dbd75347c5dd9b22f900125bb0ec4fa0dee2386
anlada <bo.hou@en-plus.com.cn>
2025-04-30 15:10:49 +0800

    feat: enhance ComponentType and ComponentVariableType with additional fields and methods

--- src/v2_1/datatypes/component.rs
+++ src/v2_1/datatypes/component.rs
@@ -5,20 +5,245 @@ use super::{custom_data::CustomDataType, evse::EVSEType};
 
 /// A physical or logical component
 #[derive(Debug, Clone, Serialize, Deserialize, Validate, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct ComponentType {
+    /// Custom data specific to this class.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub custom_data: Option<CustomDataType>,
 
+    /// Specifies the EVSE when component is located at EVSE level, also specifies the connector when component is located at Connector level.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub evse: Option<EVSEType>,
 
-    #[validate(length(max = 50))]
     /// Name of the component. Name should be taken from the list of standardized component names whenever possible.
     /// Case Insensitive. strongly advised to use Camel Case.
+    #[validate(length(max = 50))]
     pub name: String,
 
+    /// Name of instance in case the component exists as multiple instances. Case Insensitive. strongly advised to use Camel Case.
     #[serde(skip_serializing_if = "Option::is_none")]
     #[validate(length(max = 50))]
-    /// Name of instance in case the component exists as multiple instances. Case Insensitive. strongly advised to use Camel Case.
     pub instance: Option<String>,
+}
+
+impl ComponentType {
+    /// Creates a new `ComponentType` with required fields.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - Name of the component
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `ComponentType` with optional fields set to `None`
+    pub fn new(name: String) -> Self {
+        Self {
+            name,
+            custom_data: None,
+            evse: None,
+            instance: None,
+        }
+    }
+
+    /// Sets the custom data.
+    ///
+    /// # Arguments
+    ///
+    /// * `custom_data` - Custom data for this component
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn with_custom_data(mut self, custom_data: CustomDataType) -> Self {
+        self.custom_data = Some(custom_data);
+        self
+    }
+
+    /// Sets the EVSE.
+    ///
+    /// # Arguments
+    ///
+    /// * `evse` - EVSE for this component
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn with_evse(mut self, evse: EVSEType) -> Self {
+        self.evse = Some(evse);
+        self
+    }
+
+    /// Sets the instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `instance` - Instance name for this component
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn with_instance(mut self, instance: String) -> Self {
+        self.instance = Some(instance);
+        self
+    }
+
+    /// Gets the name.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the name of the component
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Sets the name.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - Name of the component
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_name(&mut self, name: String) -> &mut Self {
+        self.name = name;
+        self
+    }
+
+    /// Gets the custom data.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the custom data
+    pub fn custom_data(&self) -> Option<&CustomDataType> {
+        self.custom_data.as_ref()
+    }
+
+    /// Sets the custom data.
+    ///
+    /// # Arguments
+    ///
+    /// * `custom_data` - Custom data for this component, or None to clear
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_custom_data(&mut self, custom_data: Option<CustomDataType>) -> &mut Self {
+        self.custom_data = custom_data;
+        self
+    }
+
+    /// Gets the EVSE.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the EVSE
+    pub fn evse(&self) -> Option<&EVSEType> {
+        self.evse.as_ref()
+    }
+
+    /// Sets the EVSE.
+    ///
+    /// # Arguments
+    ///
+    /// * `evse` - EVSE for this component, or None to clear
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_evse(&mut self, evse: Option<EVSEType>) -> &mut Self {
+        self.evse = evse;
+        self
+    }
+
+    /// Gets the instance.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the instance name
+    pub fn instance(&self) -> Option<&str> {
+        self.instance.as_deref()
+    }
+
+    /// Sets the instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `instance` - Instance name for this component, or None to clear
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_instance(&mut self, instance: Option<String>) -> &mut Self {
+        self.instance = instance;
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_component() {
+        let component = ComponentType::new("Connector".to_string());
+
+        assert_eq!(component.name(), "Connector");
+        assert_eq!(component.custom_data(), None);
+        assert_eq!(component.evse(), None);
+        assert_eq!(component.instance(), None);
+    }
+
+    #[test]
+    fn test_with_methods() {
+        let custom_data = CustomDataType::new("VendorX".to_string());
+        let evse = EVSEType {
+            id: 1,
+            connector_id: Some(2),
+            custom_data: None,
+        };
+
+        let component = ComponentType::new("Connector".to_string())
+            .with_custom_data(custom_data.clone())
+            .with_evse(evse.clone())
+            .with_instance("Main".to_string());
+
+        assert_eq!(component.name(), "Connector");
+        assert_eq!(component.custom_data(), Some(&custom_data));
+        assert_eq!(component.evse(), Some(&evse));
+        assert_eq!(component.instance(), Some("Main"));
+    }
+
+    #[test]
+    fn test_setter_methods() {
+        let custom_data = CustomDataType::new("VendorX".to_string());
+        let evse = EVSEType {
+            id: 1,
+            connector_id: Some(2),
+            custom_data: None,
+        };
+
+        let mut component = ComponentType::new("Connector".to_string());
+
+        component
+            .set_name("Meter".to_string())
+            .set_custom_data(Some(custom_data.clone()))
+            .set_evse(Some(evse.clone()))
+            .set_instance(Some("Secondary".to_string()));
+
+        assert_eq!(component.name(), "Meter");
+        assert_eq!(component.custom_data(), Some(&custom_data));
+        assert_eq!(component.evse(), Some(&evse));
+        assert_eq!(component.instance(), Some("Secondary"));
+
+        // Test clearing optional fields
+        component
+            .set_custom_data(None)
+            .set_evse(None)
+            .set_instance(None);
+
+        assert_eq!(component.custom_data(), None);
+        assert_eq!(component.evse(), None);
+        assert_eq!(component.instance(), None);
+    }
 }
--- src/v2_1/datatypes/component_variable.rs
+++ src/v2_1/datatypes/component_variable.rs
@@ -5,6 +5,7 @@ use super::{component::ComponentType, custom_data::CustomDataType, variable::Var
 
 /// Class to report components, variables and variable attributes and characteristics.
 #[derive(Debug, Clone, Serialize, Deserialize, Validate, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct ComponentVariableType {
     /// Custom data specific to this class.
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -16,4 +17,183 @@ pub struct ComponentVariableType {
     /// Optional. Variable(s) for which the report is requested.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub variable: Option<VariableType>,
+}
+
+impl ComponentVariableType {
+    /// Creates a new `ComponentVariableType` with required fields.
+    ///
+    /// # Arguments
+    ///
+    /// * `component` - Component for which a report of Variable is requested
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `ComponentVariableType` with optional fields set to `None`
+    pub fn new(component: ComponentType) -> Self {
+        Self {
+            component,
+            custom_data: None,
+            variable: None,
+        }
+    }
+
+    /// Sets the custom data.
+    ///
+    /// # Arguments
+    ///
+    /// * `custom_data` - Custom data for this component variable
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn with_custom_data(mut self, custom_data: CustomDataType) -> Self {
+        self.custom_data = Some(custom_data);
+        self
+    }
+
+    /// Sets the variable.
+    ///
+    /// # Arguments
+    ///
+    /// * `variable` - Variable for which the report is requested
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn with_variable(mut self, variable: VariableType) -> Self {
+        self.variable = Some(variable);
+        self
+    }
+
+    /// Gets the component.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the component
+    pub fn component(&self) -> &ComponentType {
+        &self.component
+    }
+
+    /// Sets the component.
+    ///
+    /// # Arguments
+    ///
+    /// * `component` - Component for which a report of Variable is requested
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_component(&mut self, component: ComponentType) -> &mut Self {
+        self.component = component;
+        self
+    }
+
+    /// Gets the custom data.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the custom data
+    pub fn custom_data(&self) -> Option<&CustomDataType> {
+        self.custom_data.as_ref()
+    }
+
+    /// Sets the custom data.
+    ///
+    /// # Arguments
+    ///
+    /// * `custom_data` - Custom data for this component variable, or None to clear
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_custom_data(&mut self, custom_data: Option<CustomDataType>) -> &mut Self {
+        self.custom_data = custom_data;
+        self
+    }
+
+    /// Gets the variable.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the variable
+    pub fn variable(&self) -> Option<&VariableType> {
+        self.variable.as_ref()
+    }
+
+    /// Sets the variable.
+    ///
+    /// # Arguments
+    ///
+    /// * `variable` - Variable for which the report is requested, or None to clear
+    ///
+    /// # Returns
+    ///
+    /// Self reference for method chaining
+    pub fn set_variable(&mut self, variable: Option<VariableType>) -> &mut Self {
+        self.variable = variable;
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_component_variable() {
+        let component = ComponentType::new("Connector".to_string());
+        let component_variable = ComponentVariableType::new(component.clone());
+
+        assert_eq!(component_variable.component(), &component);
+        assert_eq!(component_variable.custom_data(), None);
+        assert_eq!(component_variable.variable(), None);
+    }
+
+    #[test]
+    fn test_with_methods() {
+        let component = ComponentType::new("Connector".to_string());
+        let custom_data = CustomDataType::new("VendorX".to_string());
+        let variable = VariableType::new(
+            "CurrentLimit".to_string(),
+            "Main".to_string(),
+        );
+
+        let component_variable = ComponentVariableType::new(component.clone())
+            .with_custom_data(custom_data.clone())
+            .with_variable(variable.clone());
+
+        assert_eq!(component_variable.component(), &component);
+        assert_eq!(component_variable.custom_data(), Some(&custom_data));
+        assert_eq!(component_variable.variable(), Some(&variable));
+    }
+
+    #[test]
+    fn test_setter_methods() {
+        let component1 = ComponentType::new("Connector".to_string());
+        let component2 = ComponentType::new("Meter".to_string());
+        let custom_data = CustomDataType::new("VendorX".to_string());
+        let variable = VariableType::new(
+            "CurrentLimit".to_string(),
+            "Secondary".to_string(),
+        );
+
+        let mut component_variable = ComponentVariableType::new(component1);
+
+        component_variable
+            .set_component(component2.clone())
+            .set_custom_data(Some(custom_data.clone()))
+            .set_variable(Some(variable.clone()));
+
+        assert_eq!(component_variable.component(), &component2);
+        assert_eq!(component_variable.custom_data(), Some(&custom_data));
+        assert_eq!(component_variable.variable(), Some(&variable));
+
+        // Test clearing optional fields
+        component_variable
+            .set_custom_data(None)
+            .set_variable(None);
+
+        assert_eq!(component_variable.custom_data(), None);
+        assert_eq!(component_variable.variable(), None);
+    }
 }
