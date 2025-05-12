90b702a9d01e63ef4dc87a15a4c740108697c046
anlada <bo.hou@en-plus.com.cn>
2025-05-12 18:16:28 +0800

    refactor(EVPowerScheduleEntryType): use Decimal for power and add validation to custom_data

--- src/v2_1/datatypes/ev_power_schedule_entry.rs
+++ src/v2_1/datatypes/ev_power_schedule_entry.rs
@@ -1,21 +1,23 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
-
+use rust_decimal::Decimal;
 use super::custom_data::CustomDataType;
 
 /// Entry in the EVPowerSchedule.
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct EVPowerScheduleEntryType {
-    /// Custom data from the Charging Station.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub custom_data: Option<CustomDataType>,
-
     /// Duration of the schedule entry in seconds.
     pub duration: i32,
 
     /// Power in Watts (W).
-    pub power: f64,
+    #[serde(with = "rust_decimal::serde::arbitrary_precision")]
+    pub power: Decimal,
+
+    /// Custom data from the Charging Station.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(nested)]
+    pub custom_data: Option<CustomDataType>,
 }
 
 impl EVPowerScheduleEntryType {
