5954b1c501388b73dac9681f2a386064b7caea71
anlang <2682525840@qq.com>
2025-05-13 22:45:06 +0800

    refactor(v2_1/datatypes): Remove redundant parameter from LimitMaxDischargeType::new

--- src/v2_1/datatypes/limit_max_discharge_get.rs
+++ src/v2_1/datatypes/limit_max_discharge_get.rs
@@ -196,7 +196,7 @@ mod tests {
     #[test]
     fn test_new_limit_max_discharge_get() {
         let pct_max_discharge_power = Decimal::from_str("80.0").unwrap();
-        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power, 75.0);
+        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power);
         let id = "setting1".to_string();
         let is_superseded = false;
         let is_default = true;
@@ -218,7 +218,7 @@ mod tests {
     #[test]
     fn test_with_custom_data() {
         let pct_max_discharge_power = Decimal::from_str("80.0").unwrap();
-        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power, 75.0);
+        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power);
         let id = "setting1".to_string();
         let is_superseded = false;
         let is_default = true;
@@ -243,8 +243,8 @@ mod tests {
     fn test_setter_methods() {
         let pct_max_discharge_power1 = Decimal::from_str("80.0").unwrap();
         let pct_max_discharge_power2 = Decimal::from_str("90.0").unwrap();
-        let limit_max_discharge1 = LimitMaxDischargeType::new(1, pct_max_discharge_power1, 75.0);
-        let limit_max_discharge2 = LimitMaxDischargeType::new(2, pct_max_discharge_power2, 85.0);
+        let limit_max_discharge1 = LimitMaxDischargeType::new(1, pct_max_discharge_power1);
+        let limit_max_discharge2 = LimitMaxDischargeType::new(2, pct_max_discharge_power2);
         let id1 = "setting1".to_string();
         let id2 = "setting2".to_string();
         let is_superseded1 = false;
@@ -282,7 +282,7 @@ mod tests {
     fn test_validate() {
         // Valid values
         let pct_max_discharge_power = Decimal::from_str("80.0").unwrap();
-        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power.clone(), 75.0);
+        let limit_max_discharge = LimitMaxDischargeType::new(1, pct_max_discharge_power.clone());
         let id = "setting1".to_string();
         let is_superseded = false;
         let is_default = true;
@@ -314,7 +314,7 @@ mod tests {
         assert!(limit_get.validate().is_err());
 
         // Test with invalid LimitMaxDischargeType (negative priority)
-        let invalid_limit_max_discharge = LimitMaxDischargeType::new(-1, pct_max_discharge_power, 75.0);
+        let invalid_limit_max_discharge = LimitMaxDischargeType::new(-1, pct_max_discharge_power);
         let limit_get = LimitMaxDischargeGetType::new(
             invalid_limit_max_discharge,
             id.clone(),
