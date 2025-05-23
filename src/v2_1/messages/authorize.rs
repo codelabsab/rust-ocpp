e2048385e2114104e978dea5e9ca0eb018ed8ed5
anlang <2682525840@qq.com>
2025-05-23 16:19:19 +0800

    Add OCSPRequestDataType and HashAlgorithmEnumType to AuthorizeRequest

--- src/v2_1/messages/authorize.rs
+++ src/v2_1/messages/authorize.rs
@@ -2,8 +2,8 @@ use serde::{Deserialize, Serialize};
 use validator::Validate;
 
 use crate::v2_1::{
-    datatypes::{CustomDataType, IdTokenInfoType, IdTokenType, TariffType},
-    enumerations::{AuthorizeCertificateStatusEnumType, EnergyTransferModeEnumType},
+    datatypes::{CustomDataType, IdTokenInfoType, IdTokenType, TariffType, OCSPRequestDataType},
+    enumerations::{AuthorizeCertificateStatusEnumType, EnergyTransferModeEnumType, HashAlgorithmEnumType},
 };
 
 /// Request to start a transaction with the given idToken.
@@ -28,48 +28,6 @@ pub struct AuthorizeRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[validate(length(min = 1, max = 4))]
     pub iso15118_certificate_hash_data: Option<Vec<OCSPRequestDataType>>,
-}
-
-/// Information about a certificate for an OCSP check.
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Validate)]
-#[serde(rename_all = "camelCase")]
-pub struct OCSPRequestDataType {
-    /// Optional. Custom data from the Charging Station.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub custom_data: Option<CustomDataType>,
-
-    /// Required. Used algorithms for the hashes provided.
-    pub hash_algorithm: HashAlgorithmEnumType,
-
-    /// Required. The hash of the issuer's distinguished name (DN), that must be calculated over the DER
-    /// encoding of the issuer's name field in the certificate being checked.
-    #[validate(length(max = 128))]
-    pub issuer_name_hash: String,
-
-    /// Required. The hash of the DER encoded public key: the value (excluding tag and length) of the subject
-    /// public key field in the issuer's certificate.
-    #[validate(length(max = 128))]
-    pub issuer_key_hash: String,
-
-    /// Required. The string representation of the hexadecimal value of the serial number without the
-    /// prefix "0x" and without leading zeroes.
-    #[validate(length(max = 40))]
-    pub serial_number: String,
-
-    /// Required. This contains the responder URL (Case insensitive).
-    #[validate(length(max = 2000))]
-    pub responder_url: String,
-}
-
-/// Used algorithms for the hashes provided.
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub enum HashAlgorithmEnumType {
-    #[serde(rename = "SHA256")]
-    SHA256,
-    #[serde(rename = "SHA384")]
-    SHA384,
-    #[serde(rename = "SHA512")]
-    SHA512,
 }
 
 /// Response to an AuthorizeRequest.
