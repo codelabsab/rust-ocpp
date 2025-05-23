4c6f752d6e9c3e70cc98486b97e9d01203d42eca
anlang <2682525840@qq.com>
2025-05-23 16:20:30 +0800

    feat: Remove redundant BatteryDataType from battery_swap.rs

--- src/v2_1/messages/battery_swap.rs
+++ src/v2_1/messages/battery_swap.rs
@@ -3,43 +3,9 @@ use serde::{Deserialize, Serialize};
 use validator::Validate;
 
 use crate::v2_1::{
-    datatypes::{CustomDataType, IdTokenType},
+    datatypes::{CustomDataType, IdTokenType, BatteryDataType},
     enumerations::BatterySwapEventEnumType,
 };
-
-/// Battery data information.
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Validate)]
-#[serde(rename_all = "camelCase")]
-pub struct BatteryDataType {
-    /// Optional. Custom data from the Charging Station.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub custom_data: Option<CustomDataType>,
-
-    /// Required. Slot number where battery is inserted or removed.
-    #[validate(range(min = 0))]
-    pub evse_id: i32,
-
-    /// Optional. Production date of battery.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub production_date: Option<DateTime<Utc>>,
-
-    /// Required. Serial number of battery.
-    #[validate(length(max = 50))]
-    pub serial_number: String,
-
-    /// Required. State of charge.
-    #[validate(range(min = 0.0, max = 100.0))]
-    pub so_c: f64,
-
-    /// Required. State of health.
-    #[validate(range(min = 0.0, max = 100.0))]
-    pub so_h: f64,
-
-    /// Optional. Vendor-specific info from battery in undefined format.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[validate(length(max = 500))]
-    pub vendor_info: Option<String>,
-}
 
 /// Request body for the BatterySwap request.
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Validate)]
